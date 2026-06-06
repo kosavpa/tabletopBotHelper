65b19bacb608fe84daec2df7cf0e1ae4a07007fc
Saveliy <kosavpa@yandex.ru>
2026-06-06 20:55:41 +0400

    Update PeriodicTask.py

--- app/PeriodicTask.py
+++ app/PeriodicTask.py
@@ -19,7 +19,10 @@ async def run_periodic_task():
 async def periodic_loop():
     while True:
         await ask_about_game()
-        await asyncio.sleep(10)
+        
+        seconds_until_next_minute = 60 - now.second - now.microsecond / 1_000_000
+        
+        await asyncio.sleep(seconds_until_next_minute)
 
 
 async def ask_about_game():
